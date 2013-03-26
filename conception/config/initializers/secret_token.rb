5b67137d85bbba5611f4eab738be04f4d9968f80
Matt Adelman <madelman@wesleyan.edu>
2013-03-26 16:17:09 -0400

    removed web app

--- conception/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Conception::Application.config.secret_token = 'cd65bd1e3c9d43a62b17f5c23dcf3d03cc707ccc2c857a8227d4921cd8957de762a4533974841b5424ac9cea5325827af3d2402acec64a5ebe49dc662f427ffc'
